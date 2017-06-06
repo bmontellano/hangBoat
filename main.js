981a393b1f2071cffcd53e508fee98117bdd3fef
Brian Montellano <bmontellano@gmail.com>
2017-06-06 14:56:12 -0700

    Updated changes to index.html, styles.css, main.js

--- main.js
+++ main.js
@@ -41,22 +41,6 @@ function moveRight2(){
 $boatDiv2.animate({'left':'+=40px'}, 800,checkWinner);
 }
 
-var gameBoard = {
-  letter1: '_',
-  letter2: '_',
-  letter3: '_',
-  letter4: '_',
-  letter5: '_',
-  letter6: '_',
-  letter7: '_',
-  letter8: '_',
-  letter9: '_',
-  letter10: '_',
-  letter11:'_',
-  letter12: '_'
-}
-
-
 var $questionButton = $('.questionButton')
 var $letter1 = $('.letter1')
 var $letter2 = $('.letter2')
@@ -76,20 +60,34 @@ var questionTurns = 0;
 
 $questionButton.on('click',questButton)
 
+function questButtonLogic(){
+  if(turns%2===0){
+      moveRight1();
+      moveRight1();
+}else {
+      moveRight2();
+      moveRight2();
+}
+}
+
+
 function questButton(){
+  if(questionTurns===0){
   var t = prompt("Answer the question correctly to steer your boat towards a gust of wind speeding you towards the island! What famous 1970's film starred a sea creature who terrified and ate beach goers?");
     if(t=='jaws'){
-      if(turns%2===0){
-      moveRight1();
-      moveRight1();
-    }else {
-      moveRight2();
-      moveRight2();
+      questButtonLogic();
     }
-    }
-   turner();
+
    questionTurns += 1;
    questions();
+ } else if (questionTurns===1){
+ var z = prompt("You've entered Bermuda's triangle. Answer the question correctly to teleport closer to the island!What movie is about blue creatures and an element called unobtanium?")
+  if(z=='avatar'){
+    questButtonLogic();
+  }
+  questionTurns += 1;
+  questions();
+ }
 }
 
 var $playerTurn = $('.playerTurn')
@@ -106,14 +104,18 @@ if (turns%2===0){
 };
 
 
-// $letter1.change(gameLogic);
-// $letter2.change(gameLogic);
-// $letter3.change(gameLogic);
-// $letter4.change(gameLogic);
-// $letter5.change(gameLogic);
-// $letter6.change(gameLogic);
-// $letter7.change(gameLogic);
-
+$letter1.change(questions);
+$letter2.change(questions);
+$letter3.change(questions);
+$letter4.change(questions);
+$letter5.change(questions);
+$letter6.change(questions);
+$letter7.change(questions);
+$letter8.change(questions);
+$letter9.change(questions);
+$letter10.change(questions);
+$letter11.change(questions);
+$letter12.change(questions);
 
 function gameLogic(){
     if(turns%2===0){
@@ -123,38 +125,57 @@ function gameLogic(){
   }
 }
 
+
+
 var $questions = $('.questions')
 function questions(){
 if (questionTurns === 0 ){
-$questions.text('What big boat breaks in half crossing the Atlantic at the turn of the 20th century?')
-if ($letter1.val() == 't'){ gameLogic() }
-if ($letter2.val() == 'i'){ gameLogic() }
-if ($letter3.val() == 't'){ gameLogic() }
-if ($letter4.val() == 'a'){ gameLogic() }
-if ($letter5.val() == 'n'){ gameLogic() }
-if ($letter6.val() == 'i'){ gameLogic() }
-if ($letter7.val() == 'c'){ gameLogic() }
+$questions.text('What big boat breaks in half crossing the Atlantic ocean at the turn of the 20th century?')
+if ($letter1.val() === 't'){ gameLogic();$letter1.val('T');return }
+if ($letter2.val() === 'i'){ gameLogic();$letter2.val('I');return }
+if ($letter3.val() === 't'){ gameLogic();$letter3.val('T');return }
+if ($letter4.val() === 'a'){ gameLogic();$letter4.val('A');return }
+if ($letter5.val() === 'n'){ gameLogic();$letter5.val('N');return }
+if ($letter6.val() === 'i'){ gameLogic();$letter6.val('I');return }
+if ($letter7.val() === 'c'){ gameLogic();$letter7.val('C');return }
 
 } else if ( questionTurns ===1){
 $questions.text("Fill in the blanks:The Last of the ....")
-if ($letter1.val() == 'm'){ gameLogic() }
-if ($letter2.val() == 'o'){ gameLogic() }
-if ($letter3.val() == 'h'){ gameLogic() }
-if ($letter4.val() == 'i'){ gameLogic() }
-if ($letter5.val() == 'c'){ gameLogic() }
-if ($letter6.val() == 'a'){ gameLogic() }
-if ($letter7.val() == 'n'){ gameLogic() }
-if ($letter8.val() == 's'){ gameLogic() }
+if ($letter1.val() == 'm'){ gameLogic();$letter1.val('M');return }
+if ($letter2.val() == 'o'){ gameLogic();$letter2.val('O');return }
+if ($letter3.val() == 'h'){ gameLogic();$letter3.val('H');return }
+if ($letter4.val() == 'i'){ gameLogic();$letter4.val('I');return }
+if ($letter5.val() == 'c'){ gameLogic();$letter5.val('C');return }
+if ($letter6.val() == 'a'){ gameLogic();$letter6.val('A');return }
+if ($letter7.val() == 'n'){ gameLogic();$letter7.val('N');return }
+if ($letter8.val() == 's'){ gameLogic();$letter8.val('S');return }
+
+} else if ( questionTurns ===2){
+  $questions.text("A young woman, who is a perfect being, crashes into a cab driver's world and asks 'Help Meee' in this 1995 future flick")
+if ($letter1.val() == 'f'){ gameLogic();$letter1.val('F');return }
+if ($letter2.val() == 'i'){ gameLogic();$letter2.val('I');return }
+if ($letter3.val() == 'f'){ gameLogic();$letter3.val('F');return }
+if ($letter4.val() == 't'){ gameLogic();$letter4.val('T');return }
+if ($letter5.val() == 'h'){ gameLogic();$letter5.val('H');return }
+if ($letter6.val() == 'e'){ gameLogic();$letter6.val('E');return }
+if ($letter7.val() == 'l'){ gameLogic();$letter7.val('L');return }
+if ($letter8.val() == 'e'){ gameLogic();$letter8.val('E');return }
+if ($letter9.val() == 'm'){ gameLogic();$letter9.val('M');return }
+if ($letter10.val() == 'e'){ gameLogic();$letter10.val('E');return }
+if ($letter11.val() == 'n'){ gameLogic();$letter11.val('N');return }
+if ($letter12.val() == 't'){ gameLogic();$letter12.val('T');return }
+  }
+  turner();
 }
-}
-setInterval(questions,500);
+questions();
+questions();
 
 function checkWinner(){
   console.log('checking winner')
-if ($boatDiv1.css('left')=='413px'){
+if ($boatDiv1.css('left')=='412.997px'){
   $boatDiv1.fadeOut(1000,function(){alert("Player 1 wins")});
 
-} else if ($boatDiv2.css('left')=='413px'){
+} else if ($boatDiv2.css('left')=='412.997px'){
   $boatDiv2.fadeOut(1000,function(){alert("Player 2 wins")});
 }
 }
